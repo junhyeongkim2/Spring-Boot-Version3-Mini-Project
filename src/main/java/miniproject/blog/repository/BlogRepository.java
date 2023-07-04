da82dfb415d38b3bb59b7ca3b331ee4fde17e930
jun <ggprgrkjh@naver.com>
2023-07-04 21:36:41 +0900

    feat: BlogRepository with JpaRepository

---
+++ src/main/java/miniproject/blog/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package miniproject.blog.repository;
+
+import miniproject.blog.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article,Long> {
+}
