9be42a32453302b1417a1c74cda7771faf55236c
jun <ggprgrkjh@naver.com>
2023-07-04 21:47:23 +0900

    feat: BlogApiController addArticle

---
+++ src/main/java/miniproject/blog/controller/BlogApiController.java
@@ -0,0 +1,27 @@
+package miniproject.blog.controller;
+
+import lombok.RequiredArgsConstructor;
+import miniproject.blog.domain.Article;
+import miniproject.blog.dto.AddArticleRequest;
+import miniproject.blog.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+
+    }
+
+}
