bbdc594b3e6c64336985c23a5f0fb9ddf4df6907
jun <ggprgrkjh@naver.com>
2023-07-06 23:20:17 +0900

    feat: data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into ARTICLE (title,content) values ('제목 1', '내용 1')
+insert into ARTICLE (title,content) values ('제목 2', '내용 2')
+insert into ARTICLE (title,content) values ('제목 3', '내용 3')
+insert into ARTICLE (title,content) values ('제목 4', '내용 4')
