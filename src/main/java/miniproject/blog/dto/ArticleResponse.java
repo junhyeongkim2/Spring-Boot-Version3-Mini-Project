3b34b6673c5463ab2602368cf0bfefbecacffe9a
jun <ggprgrkjh@naver.com>
2023-07-06 22:57:43 +0900

    feat: ArticleResponse Dto

---
+++ src/main/java/miniproject/blog/dto/ArticleResponse.java
@@ -0,0 +1,16 @@
+package miniproject.blog.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+
+    public ArticleResponse(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
