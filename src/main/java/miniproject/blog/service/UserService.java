943b39a03e9db268107bc7d336339a2bbbf03840
jun <ggprgrkjh@naver.com>
2023-07-09 17:14:41 +0900

    feat: userService save

---
+++ src/main/java/miniproject/blog/service/UserService.java
@@ -0,0 +1,25 @@
+package miniproject.blog.service;
+
+import lombok.RequiredArgsConstructor;
+import miniproject.blog.domain.User;
+import miniproject.blog.dto.AddUserRequest;
+import miniproject.blog.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+
+    }
+
+}
