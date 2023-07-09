406ab701d7c60292a70398588c63c9c53cfbd871
jun <ggprgrkjh@naver.com>
2023-07-09 16:42:47 +0900

    feat: UserRepository with JpaRepository

---
+++ src/main/java/miniproject/blog/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package miniproject.blog.repository;
+
+import miniproject.blog.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository  extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
