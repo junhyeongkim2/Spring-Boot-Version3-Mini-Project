152c95202f94221b0072a7faf88c6df603df4623
jun <ggprgrkjh@naver.com>
2023-07-09 16:04:50 +0900

    feat: modifyButton js

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,8 +1,9 @@
 
 const deleteButton = document.getElementById('delete-btn');
+const modifyButton = document.getElementById('modify-btn');
+
 
 if(deleteButton){
-
     deleteButton.addEventListener('click',event=>{
         let id = document.getElementById('article-id').value;
         fetch(`/api/articles/${id}`,{
@@ -12,7 +13,28 @@ if(deleteButton){
                 location.replace('/articles');
             });
     });
+}
 
 
+if(modifyButton){
+    modifyButton.addEventListener('click', event =>{
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`,{
+            method:'PUT',
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body: JSON.stringfy({
+                title: document.getElementById('title').value;
+                content: document.getElementById('content').value;
+            })
+        })
+            .then(()=>{
+                alert('수정이 완료되었습니다.');
+                location.replace(`/articles/${id}`);
+            });
+    });
 
 }
