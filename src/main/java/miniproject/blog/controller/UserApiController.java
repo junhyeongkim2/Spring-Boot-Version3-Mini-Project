4811b58175c18e67b6ba72bc8f972348a8121691
jun <ggprgrkjh@naver.com>
2023-07-09 17:24:33 +0900

    feat: UserApiController logout

--- src/main/java/miniproject/blog/controller/UserApiController.java
+++ src/main/java/miniproject/blog/controller/UserApiController.java
@@ -1,10 +1,15 @@
 package miniproject.blog.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import miniproject.blog.dto.AddUserRequest;
 import miniproject.blog.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -16,6 +21,14 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request){
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request,response,
+                SecurityContextHolder.getContext().getAuthentication()
+                );
         return "redirect:/login";
     }
 
