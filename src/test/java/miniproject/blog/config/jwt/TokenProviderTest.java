f414170eb60e86e2378bc7e88c4924d374881b2b
jun <ggprgrkjh@naver.com>
2023-07-10 21:49:29 +0900

    feat: generateToken test

---
+++ src/test/java/miniproject/blog/config/jwt/TokenProviderTest.java
@@ -0,0 +1,51 @@
+package miniproject.blog.config.jwt;
+
+import io.jsonwebtoken.Jwts;
+import miniproject.blog.domain.User;
+import miniproject.blog.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TokenProviderTest {
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @DisplayName("generateToken(): 유저 정보와 만료 기간을 전달해 토큰을 만들 수 있다.")
+    @Test
+    void generateToken(){
+        //given
+        User testUser = userRepository.save(User.builder()
+                .email("user@gmail.com")
+                .password("test")
+                .build());
+        //when
+        String token = tokenProvider.generateToken(testUser, Duration.ofDays(14));
+        //then
+        Long userId = Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id",Long.class);
+
+        assertThat(userId).isEqualTo(testUser.getId());
+    }
+
+
+
+
+}
