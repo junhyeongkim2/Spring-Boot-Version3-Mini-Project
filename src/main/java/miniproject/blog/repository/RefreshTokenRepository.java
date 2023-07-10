8984454a5b5c582e08c20a720f7230baf8e6314f
jun <ggprgrkjh@naver.com>
2023-07-10 22:07:06 +0900

    feat: RefreshTokenRepository

---
+++ src/main/java/miniproject/blog/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package miniproject.blog.repository;
+
+import miniproject.blog.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository  extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(Long userId);
+
+}
