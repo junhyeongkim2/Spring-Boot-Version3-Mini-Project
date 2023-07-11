0b1c89c9fd6e98cd0f42221e6a82a31838870205
jun <ggprgrkjh@naver.com>
2023-07-11 19:14:16 +0900

    feat: TokenApiController createNewAccessToken

---
+++ src/main/java/miniproject/blog/controller/TokenApiController.java
@@ -0,0 +1,33 @@
+package miniproject.blog.controller;
+
+import lombok.RequiredArgsConstructor;
+import miniproject.blog.dto.CreateAccessTokenRequest;
+import miniproject.blog.dto.CreateAccessTokenResponse;
+import miniproject.blog.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+
+    private final TokenService tokenService;
+
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken
+            (@RequestBody CreateAccessTokenRequest request){
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+
+    }
+
+
+}
