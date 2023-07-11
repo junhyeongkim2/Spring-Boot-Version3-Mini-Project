d832a38b439db1d5b1e90e620ab8b5ecd6a7793d
jun <ggprgrkjh@naver.com>
2023-07-11 18:59:49 +0900

    feat: UserService findById

--- src/main/java/miniproject/blog/service/UserService.java
+++ src/main/java/miniproject/blog/service/UserService.java
@@ -22,4 +22,11 @@ public class UserService {
 
     }
 
+    public User findById(Long userId){
+        return userRepository.findById(userId)
+                .orElseThrow(()-> new IllegalArgumentException("Unexpected user"));
+    }
+
+
+
 }
