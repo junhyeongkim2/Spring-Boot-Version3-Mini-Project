4ef2b9e2fce895c443a0cc7ff889be6fdba48f17
jun <ggprgrkjh@naver.com>
2023-07-12 19:47:55 +0900

    feat: CookieUtil deleteCookie

--- src/main/java/miniproject/blog/util/CookieUtil.java
+++ src/main/java/miniproject/blog/util/CookieUtil.java
@@ -1,7 +1,11 @@
 package miniproject.blog.util;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.SerializationUtils;
+
+import java.util.Base64;
 
 public class CookieUtil {
 
@@ -12,6 +16,25 @@ public class CookieUtil {
         cookie.setMaxAge(maxAge);
         response.addCookie(cookie);
     }
+
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name){
+        Cookie[] cookies = request.getCookies();
+        if(cookies == null){
+            return;
+        }
+
+        for(Cookie cookie : cookies){
+            if(name.equals(cookie.getName())){
+                cookie.setValue("");;
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+    }
+
+
+
 
 
 
