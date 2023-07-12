71ff49e6c8d29801ec0e828b094dcb2b8e774c31
jun <ggprgrkjh@naver.com>
2023-07-12 23:01:23 +0900

    feat: UserService findByEmail

--- src/main/java/miniproject/blog/service/UserService.java
+++ src/main/java/miniproject/blog/service/UserService.java
@@ -27,6 +27,11 @@ public class UserService {
                 .orElseThrow(()-> new IllegalArgumentException("Unexpected user"));
     }
 
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(()->new IllegalArgumentException("Unexpected user"));
+    }
+
 
 
 }
