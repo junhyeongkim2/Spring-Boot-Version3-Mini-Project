ced4fb4ad52fd7be7f1f41b1fbcbd4a7ced6e15a
jun <ggprgrkjh@naver.com>
2023-07-13 17:34:33 +0900

    feat: Article add author

--- src/main/java/miniproject/blog/domain/Article.java
+++ src/main/java/miniproject/blog/domain/Article.java
@@ -21,6 +21,11 @@ public class Article {
     @Column(name="id",updatable = false)
     private Long id;
 
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+
     @Column(name="title",nullable=false)
     private String title;
 
@@ -39,7 +44,8 @@ public class Article {
 
 
     @Builder
-    public Article(String title, String content){
+    public Article(String author, String title, String content){
+        this.author = author;
         this.title = title;
         this.content = content;
     }
