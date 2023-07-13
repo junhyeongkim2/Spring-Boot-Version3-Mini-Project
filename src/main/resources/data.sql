44ae5f8264827c5b1fd809cf6d1f15cb122caff9
jun <ggprgrkjh@naver.com>
2023-07-13 17:46:16 +0900

    feat: data.sql add author

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into ARTICLE (title,content,created_at,updated_at) values ('제목 1', '내용 1',NOW(),NOW())
-insert into ARTICLE (title,content,created_at,updated_at) values ('제목 2', '내용 2',NOW(),NOW())
-insert into ARTICLE (title,content,created_at,updated_at) values ('제목 3', '내용 3',NOW(),NOW())
-insert into ARTICLE (title,content,created_at,updated_at) values ('제목 4', '내용 4',NOW(),NOW())
+insert into ARTICLE (title,content,author,created_at,updated_at) values ('제목 1', '내용 1', 'user1', NOW(),NOW())
+insert into ARTICLE (title,content,author,created_at,updated_at) values ('제목 2', '내용 2', 'user2', NOW(),NOW())
+insert into ARTICLE (title,content,author,created_at,updated_at) values ('제목 3', '내용 3', 'user3', NOW(),NOW())
+insert into ARTICLE (title,content,author,created_at,updated_at) values ('제목 4', '내용 4', 'user4', NOW(),NOW())
